f84295e728615b21b9066623379ec259cf05bacf
Алексей Соколов <aasokolov@sbercity.ru>
2025-08-06 22:25:46 +0300

    добавление mongoDb в InventoryService

--- inventory/internal/repository/part/repository.go
+++ inventory/internal/repository/part/repository.go
@@ -1,21 +1,41 @@
 package part
 
 import (
-	"sync"
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	def "github.com/alexis871aa/microservices-rocket-factory/inventory/internal/repository"
-	repoModel "github.com/alexis871aa/microservices-rocket-factory/inventory/internal/repository/model"
 )
 
 var _ def.PartRepository = (*repository)(nil)
 
 type repository struct {
-	mu   sync.RWMutex
-	data map[string]repoModel.Part
+	collection *mongo.Collection
 }
 
-func NewRepository() *repository {
+func NewRepository(db *mongo.Database) *repository {
+	collection := db.Collection("parts")
+
+	indexModels := []mongo.IndexModel{
+		{
+			Keys:    bson.D{{Key: "name", Value: 1}},
+			Options: options.Index().SetUnique(false),
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	_, err := collection.Indexes().CreateMany(ctx, indexModels)
+	if err != nil {
+		panic(err)
+	}
+
 	return &repository{
-		data: make(map[string]repoModel.Part),
+		collection: collection,
 	}
 }
