fac15d6138ea4fde12033ba58b34d5bda51053ad
Алексей Соколов <aasokolov@sbercity.ru>
2025-08-09 00:18:40 +0300

    правки после ревью - правка миграций, удаление работы с расширениями uuid, так как используется новый образ postgres18

--- order/migrations/20250802235935_create_orders_table.sql
+++ order/migrations/20250802235935_create_orders_table.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 create table orders (
-    order_uuid uuid primary key default uuid_generate_v4(),
+    order_uuid uuid primary key default uuidv7(),
     user_uuid uuid not null,
     part_uuids text[] not null,
     total_price decimal(10, 2) not null,
