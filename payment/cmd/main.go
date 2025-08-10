8e5a94551df248b532a7b04ecd9476fa0577f87d
Алексей Соколов <aasokolov@sbercity.ru>
2025-08-10 22:32:27 +0300

    реализация конфига в paymentService

--- payment/cmd/main.go
+++ payment/cmd/main.go
@@ -12,14 +12,20 @@ import (
 	"google.golang.org/grpc/reflection"
 
 	paymentV1API "github.com/alexis871aa/microservices-rocket-factory/payment/internal/api/payment/v1"
+	"github.com/alexis871aa/microservices-rocket-factory/payment/internal/config"
 	paymentService "github.com/alexis871aa/microservices-rocket-factory/payment/internal/service/payment"
 	paymentV1 "github.com/alexis871aa/microservices-rocket-factory/shared/pkg/proto/payment/v1"
 )
 
-const grpcPort = 50052
+const configPath = "./deploy/compose/payment/.env"
 
 func main() {
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	err := config.Load(configPath)
+	if err != nil {
+		panic(fmt.Errorf("failed to load config: %w", err))
+	}
+
+	lis, err := net.Listen("tcp", config.AppConfig().PaymentGRPC.Address())
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 		return
@@ -40,7 +46,7 @@ func main() {
 	reflection.Register(s)
 
 	go func() {
-		log.Printf("starting PaymentService server on port %d", grpcPort)
+		log.Printf("starting PaymentService server on port %s", config.AppConfig().PaymentGRPC.Port())
 		err = s.Serve(lis)
 		if err != nil {
 			log.Fatalf("failed to serve: %v", err)
