9b5d05049a1662ebd222560f954db4c8f0a58db0
Алексей Соколов <aasokolov@sbercity.ru>
2025-08-22 21:04:37 +0300

    имплементация kafka в платформенной библиотеке

---
+++ platform/pkg/kafka/message.go
@@ -0,0 +1,17 @@
+package kafka
+
+import "time"
+
+// Message - универсальная обёртка над сообщение Kafka
+
+type Message struct {
+	Headers        map[string][]byte
+	Timestamp      time.Time
+	BlockTimestamp time.Time
+
+	Key       []byte
+	Value     []byte
+	Topic     string
+	Partition int32
+	Offset    int64
+}
