39870288c1d21b187ac4247974e7c6508937f842
Алексей Соколов <aasokolov@sbercity.ru>
2025-08-24 01:21:08 +0300

    написание main.go

---
+++ assembly/cmd/main.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"go.uber.org/zap"
+
+	"github.com/alexis871aa/microservices-rocket-factory/assembly/internal/config"
+	"github.com/alexis871aa/microservices-rocket-factory/platform/pkg/closer"
+	"github.com/alexis871aa/microservices-rocket-factory/platform/pkg/logger"
+)
+
+const configPath = "./deploy/compose/inventory/.env"
+
+func main() {
+	err := config.Load(configPath)
+	if err != nil {
+		panic(fmt.Errorf("failed to load config: %w", err))
+	}
+
+	appCtx, appCancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer appCancel()
+	defer gracefulShutdown()
+
+	closer.Configure(syscall.SIGINT, syscall.SIGTERM)
+}
+
+func gracefulShutdown() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := closer.CloseAll(ctx); err != nil {
+		logger.Error(ctx, "❌ Ошибка при завершении работы", zap.Error(err))
+	}
+}
