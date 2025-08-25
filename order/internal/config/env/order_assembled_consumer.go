12466964fe3fb3e665e668c1f033cec31ca7c6a1
Алексей Соколов <aasokolov@sbercity.ru>
2025-08-25 23:46:05 +0300

    OrderService правка config

--- order/internal/config/env/order_assembled_consumer.go
+++ order/internal/config/env/order_assembled_consumer.go
@@ -6,8 +6,8 @@ import (
 )
 
 type orderAssembledConsumerEnvConfig struct {
-	TopicName string `env:"ORDER_ASSEMBLED_TOPIC_NAME, required"`
-	GroupID   string `env:"ORDER_ASSEMBLED_CONSUMER_GROUP_ID, required"`
+	TopicName string `env:"ORDER_ASSEMBLED_TOPIC_NAME,required"`
+	GroupID   string `env:"ORDER_ASSEMBLED_CONSUMER_GROUP_ID,required"`
 }
 
 type orderAssembledConsumerConfig struct {
