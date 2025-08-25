329652c9ef64d7a194889d01ea46dcc2548b1e40
Алексей Соколов <aasokolov@sbercity.ru>
2025-08-25 23:48:40 +0300

    OrderService добавление консьюмера

---
+++ order/internal/service/consumer/order_consumer/consumer.go
@@ -0,0 +1,38 @@
+package order_consumer
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+
+	kafkaConverter "github.com/alexis871aa/microservices-rocket-factory/order/internal/converter/kafka"
+	def "github.com/alexis871aa/microservices-rocket-factory/order/internal/service"
+	"github.com/alexis871aa/microservices-rocket-factory/platform/pkg/kafka"
+	"github.com/alexis871aa/microservices-rocket-factory/platform/pkg/logger"
+)
+
+var _ def.OrderConsumerService = (*service)(nil)
+
+type service struct {
+	orderConsumer             kafka.Consumer
+	orderShipAssembledDecoder kafkaConverter.OrderAssembledDecoder
+}
+
+func NewService(orderConsumer kafka.Consumer, orderShipAssembledDecoder kafkaConverter.OrderAssembledDecoder) *service {
+	return &service{
+		orderConsumer:             orderConsumer,
+		orderShipAssembledDecoder: orderShipAssembledDecoder,
+	}
+}
+
+func (s *service) RunConsumer(ctx context.Context) error {
+	logger.Info(ctx, "Starting order consumer service")
+
+	err := s.orderConsumer.Consume(ctx, s.OrderHandler)
+	if err != nil {
+		logger.Error(ctx, "Consume from order.assembled topic error", zap.Error(err))
+		return err
+	}
+
+	return nil
+}
