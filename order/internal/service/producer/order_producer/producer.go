26ac78b05f9be4dd38b4ddbb399988f83e7c5639
Алексей Соколов <aasokolov@sbercity.ru>
2025-08-25 23:49:07 +0300

    OrderService добавление продюсера и создание новых сервисов продюсера и консьюмера

---
+++ order/internal/service/producer/order_producer/producer.go
@@ -0,0 +1,46 @@
+package order_producer
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+	"google.golang.org/protobuf/proto"
+
+	"github.com/alexis871aa/microservices-rocket-factory/order/internal/model"
+	"github.com/alexis871aa/microservices-rocket-factory/platform/pkg/kafka"
+	"github.com/alexis871aa/microservices-rocket-factory/platform/pkg/logger"
+	eventsV1 "github.com/alexis871aa/microservices-rocket-factory/shared/pkg/proto/events/v1"
+)
+
+type service struct {
+	orderProducer kafka.Producer
+}
+
+func NewService(orderProducer kafka.Producer) *service {
+	return &service{
+		orderProducer: orderProducer,
+	}
+}
+
+func (p *service) ProduceOrderPaid(ctx context.Context, event model.OrderPaid) error {
+	msg := &eventsV1.OrderPaid{
+		EventUuid:       event.EventUUID,
+		OrderUuid:       event.OrderUUID,
+		UserUuid:        event.UserUUID,
+		PaymentMethod:   event.PaymentMethod,
+		TransactionUuid: event.TransactionUUID,
+	}
+
+	payload, err := proto.Marshal(msg)
+	if err != nil {
+		logger.Error(ctx, "failed to marshal order paid")
+	}
+
+	err = p.orderProducer.Send(ctx, []byte(event.OrderUUID), payload)
+	if err != nil {
+		logger.Error(ctx, "failed to publish order paid", zap.Any("event", event), zap.Error(err))
+		return err
+	}
+
+	return nil
+}
