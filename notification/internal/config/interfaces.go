ff69cf882325d6516fbebb79711735ee87a373ca
Алексей Соколов <aasokolov@sbercity.ru>
2025-08-28 01:15:53 +0300

    NotificationService имплементация и настройка конфигурации

---
+++ notification/internal/config/interfaces.go
@@ -0,0 +1,24 @@
+package config
+
+import "github.com/IBM/sarama"
+
+type LoggerConfig interface {
+	Level() string
+	AsJson() bool
+}
+
+type OrderPaidConsumerConfig interface {
+	Topic() string
+	GroupID() string
+	Config() *sarama.Config
+}
+
+type OrderAssembledConsumerConfig interface {
+	Topic() string
+	GroupID() string
+	Config() *sarama.Config
+}
+
+type KafkaConfig interface {
+	Brokers() []string
+}
